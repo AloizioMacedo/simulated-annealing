ed111b722633ac4551f0f53cf2e5b8a87b70ca6a
AloizioMacedo <atsam2@hotmail.com>
2024-03-13 21:54:37 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use rand::{
+    distributions::{Distribution, Uniform},
+    thread_rng,
+};
+
+trait State {
+    fn acceptability(&self, new: &Self, t: f64) -> f64;
+
+    fn get_next_states(&self) -> impl Iterator<Item = Self>
+    where
+        Self: Clone;
+}
+
+fn simulated_annealing<S, T>(state: &S, max_k: usize, temperature: T) -> S
+where
+    S: State + Clone,
+    T: Fn(usize) -> f64,
+{
+    let mut rng = thread_rng();
+    let uniform = Uniform::new_inclusive(0.0, 1.0);
+
+    let mut current_state = state.clone();
+
+    for k in 0..max_k {
+        let t = temperature(k);
+
+        let next_state = state
+            .get_next_states()
+            .find(|next_state| state.acceptability(next_state, t) >= uniform.sample(&mut rng));
+
+        if let Some(next_state) = next_state {
+            current_state = next_state;
+        } else {
+            break;
+        }
+    }
+
+    current_state
+}
+
+fn main() {
+    println!("Hello, world!");
+}
