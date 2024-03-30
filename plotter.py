abcd1c11546d93c2b354b73b19a3bcfe631fca20
AloizioMacedo <atsam2@hotmail.com>
2024-03-30 04:42:29 -0300

    Clean plotter.py

--- plotter.py
+++ plotter.py
@@ -1,44 +1,31 @@
-import json
-import plotly.graph_objects as go
-from websockets.sync.client import connect
-from dash import Dash, html, dcc, callback, Input, Output
+from dash_extensions import WebSocket
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 
 
 f = px.scatter(x=[1], y=[1])
 f.update_traces(mode="markers+lines")
+f.update_xaxes(visible=False)
+f.update_yaxes(visible=False)
 f.layout.title = "TSP"
 
+update_graph = """function(msg) {
+    if(!msg){return {};}  // no data, just return
+    const data = JSON.parse(msg.data);  // read the data
+    return {data: [{x: data.x, y: data.y, type: "scatter", mode: "markers+lines"}]}};  // plot the data
+"""
 
 app = Dash()
 app.layout = html.Div(
     [
+        WebSocket(id="ws", url="ws://localhost:3000/ws"),
         dcc.Graph(id="figure", figure=f),
-        dcc.Interval(id="interval", interval=200, n_intervals=2000),
     ]
 )
-
-
-@callback(
-    Output(component_id="figure", component_property="figure"),
-    Input(component_id="interval", component_property="n_intervals"),
+app.clientside_callback(
+    update_graph, Output("figure", "figure"), Input("ws", "message")
 )
-def hello(n_intervals):
-    try:
-        message = websocket.recv()
-        points = json.loads(message)
-        x = points["x"]
-        y = points["y"]
-
-        f = px.scatter(x=x, y=y)
-        f.update_traces(mode="markers+lines")
-
-        return f
-    except Exception as e:
-        print(e)
-        pass
 
 
 if __name__ == "__main__":
-    with connect("ws://localhost:3000/ws") as websocket:
-        app.run_server(debug=True, use_reloader=False)
+    app.run_server(debug=True, use_reloader=False)
